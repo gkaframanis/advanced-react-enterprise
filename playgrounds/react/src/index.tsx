8b8e0c206e40fd24b497472d0ca61d9e313ca766
KAFRAMANIS Gregory <Gregory.KAFRAMANIS@intrasoft-intl.com>
2022-09-23 14:25:23 +0300

    Testing the Margin component in playground

--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import { Color } from "@ds.e/react";
-import { Text } from "@ds.e/react";
+import { Text, Margin } from "@ds.e/react";
 
 import '@ds.e/scss/lib/Utilities.css';
 import '@ds.e/scss/lib/Text.css';
-
+import '@ds.e/scss/lib/Margin.css';
+import '@ds.e/scss/lib/global.css';
 
 // ReactDOM.render(
 //     <Color hexCode="#000" />,
@@ -14,6 +15,10 @@ import '@ds.e/scss/lib/Text.css';
 // )
 
 ReactDOM.render(
-    <Text size="base">This is some text</Text>,
+    <div>
+        <Margin>
+            <Text size="base">This is some text</Text>
+        </Margin>
+    </div>,
     document.querySelector('#root')
 )
