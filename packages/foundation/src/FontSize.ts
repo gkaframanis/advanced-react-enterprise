b382210b5ac1522fda813c05fe6ecb0d637e2d4f
KAFRAMANIS Gregory <Gregory.KAFRAMANIS@intrasoft-intl.com>
2022-09-28 09:29:36 +0300

    fix: checking lint-staged

--- packages/foundation/src/FontSize.ts
+++ packages/foundation/src/FontSize.ts
@@ -1,9 +1,12 @@
 const fontSizes = {
-    xs: 'xs',
-    sm: 'sm',
+    xs: 'xs',   sm: 'sm',
     base: 'base',
     lg: 'lg',
     xl: 'xl',
 };
+
+
+
+
 
 export default Object.freeze(fontSizes);
